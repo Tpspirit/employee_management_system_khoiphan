fc10d6eda02a872df1a88e89ede0cc631b3010d1
Phan Khoi <giakhoi2015@gmail.com>
2024-11-28 04:14:21 +0200

    Add 'edit', 'delete' and 'add new employee' functions

--- src/main/java/fi/haagahelia/employee_management_system_khoiphan/web/EmployeeController.java
+++ src/main/java/fi/haagahelia/employee_management_system_khoiphan/web/EmployeeController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.employee_management_system_khoiphan.domain.DepartmentRepository;
+import fi.haagahelia.employee_management_system_khoiphan.domain.Employee;
 import fi.haagahelia.employee_management_system_khoiphan.domain.EmployeeRepository;
 
 @Controller
@@ -16,12 +19,43 @@ public class EmployeeController {
     private EmployeeRepository repository;
 
     @Autowired
-    private DepartmentRepository categoryrepository;
+    private DepartmentRepository departmentrepository;
     
 
+    // Show employee list
     @GetMapping({"/","/employeelist"})
     public String employeelist(Model model){
         model.addAttribute("employees", repository.findAll());
         return "employeelist";
+    }
+
+    // Add employee
+    @GetMapping("/add")
+    public String add(Model model){
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("departments", departmentrepository.findAll());
+        return "addemployee";
+    }
+
+    @PostMapping("/add")
+    public String add(Employee employee){
+        repository.save(employee);
+        return "redirect:/employeelist";
+    }
+
+
+    // Edit employee
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model){
+        model.addAttribute("employee", repository.findById(id));
+        model.addAttribute("departments", departmentrepository.findAll());
+        return "editemployee";
+    }
+
+    // Delete
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        repository.deleteById(id);
+        return "redirect:../employeelist";
     }
 }
